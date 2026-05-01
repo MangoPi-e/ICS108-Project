c57db47117c5b667145916ed054cf23347cd0263
Mango <themango.2007@gmail.com>
2026-05-01 09:44:36 +0300

    ultra Fixing
    Fixed most of the things

--- ICS108Project/src/DataStructures.java
+++ ICS108Project/src/DataStructures.java
@@ -1,105 +1,245 @@
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.TreeMap;
-
 public class DataStructures
 {
-    public static String[] EventTypes = {"Sports", "Social", "Religious", "Academic"};
-    public static String[] VenueTypes = {"Sports area", "Lecture hall", "Conference hall", "Public space"};
+    public static final String[] EventTypes = {
+            "Sports",
+            "Social",
+            "Religious",
+            "Academic"
+    };
+    public static final String[] VenueTypes = {
+            "Sports Area",
+            "Lecture Hall",
+            "Conference Hall",
+            "Public Space"
+    };
 }
 
-class Event
-{//initiating the class properties
-    String Name, Type;
-    MyDate StartingDate, EndDate;
-    Time time;
-    Venue venue;
-    public Event(String name, MyDate startingDate, MyDate endDate, Time time, int TypeIndex, Venue venue)
-    {
-        this.Name = name;
-        this.StartingDate = startingDate;
-        this.EndDate = endDate;
-        this.time = time;
-        this.Type = DataStructures.EventTypes[TypeIndex];
+
+class Event {
+    private String name;
+    private String type;
+    private DateTime start;
+    private DateTime end;
+    private Venue venue;
+    private Department department;
+
+    public Event(
+            String name,
+            DateTime start,
+            DateTime end,
+            int typeIndex,
+            Venue venue,
+            Department department
+    ) {
+        this.name = name;
+        this.start = start;
+        this.end = end;
+        this.type = DataStructures.EventTypes[typeIndex];
         this.venue = venue;
+        this.department = department;
+    }
+
+
+    public String get_name() {return name;}
+    public String get_type() {return type;}
+    public DateTime get_start() {return start;}
+    public DateTime get_end() {return end;}
+    public Venue get_venue() {return venue;}
+    public Department get_department() {return department;}
+
+    public void set_name(String name) {this.name = name;}
+    public void set_type(int typeIndex) {this.type = DataStructures.EventTypes[typeIndex];}
+    public void set_start(DateTime start) {this.start = start;}
+    public void set_end(DateTime end) {this.end = end;}
+    public void set_venue(Venue venue) {this.venue = venue;}
+    public void set_department(Department department) {this.department = department;}
+
+
+    @Override
+    public String toString() {
+        return "Event: " + name + "\nType: " + type +
+                "\nStart: " + start + "\nEnd: " + end +
+                "\nVenue: " + venue + "\nDepartment: " + department +
+                "\n----------------------";
     }
 }
 
-class Venue
-{
-    String Name, Type;
-    ArrayList<MyDate> Dates = new ArrayList<MyDate>();
-    Venue(String Name, int TypeIndex)
-    {
-        this.Name = Name;
-        this.Type = DataStructures.VenueTypes[TypeIndex];
-    }
-}
 
-class MyDate implements Comparable<MyDate>
-{
-    int day, month, year, noDate;
-    public MyDate(int day, int month, int year)
-    {
+class MyDate implements Comparable<MyDate> {
+    private int day, month, year, value;
+
+    public MyDate(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
-        this.noDate = day + (month*100) + (year*10000);
+        this.value = year * 10000 + month * 100 + day;//easy to use value for comparing datas
     }
 
-    static int  MaxDays(int year, int month)
-        {
-            int[] DaysPerMonth = {31, -1, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-            if (month == 2)
-            {
-                if (year%400 == 0){return 29;}
-                if (year%100 == 0){return 28;}
-                if (year%4 == 0){return 29;}
-                return 28;
-            }
-            else{return DaysPerMonth[month-1];}
+    public static int max_days(int year, int month) {
+        int[] days = {31, -1, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};//array of days per-month excluding February
+
+        if (month == 2) {/*year test for February to check whether it's 28 or 29 days*/
+            if (year % 400 == 0) return 29;
+            if (year % 100 == 0) return 28;
+            if (year % 4 == 0) return 29;
+            return 28;
         }
 
-    public int getDay() {return day;}
-    public void setDay(int day) {this.day = day;this.noDate = day + (month*100) + (year*10000);}
-
-    public int getMonth() {return month;}
-    public void setMonth(int month) {this.month = month;this.noDate = day + (month*100) + (year*10000);}
-
-    public int getYear() {return year;}
-    public void setYear(int year) {this.year = year;this.noDate = day + (month*100) + (year*10000);}
-
-    @Override
-    public String toString()
-    {
-        return day + "/" + month + "/" + year;
+        return days[month - 1];
     }
+
+
+
+    public int get_day() { return day; }
+    public int get_month() { return month; }
+    public int get_year() { return year; }
+
+    //Both setters update the corresponding parameter and the value
+    public void set_day(int day) {this.day = day;this.value = year * 10000 + month * 100 + day;}
+    public void set_month(int month) {this.month = month;this.value = year * 10000 + month * 100 + day;}
+    public void set_year(int year) {this.year = year;this.value = year * 10000 + month * 100 + day;}
+
+
+
     @Override
-    public int compareTo(MyDate other)
-    {
-        return this.noDate - other.noDate;
+    public int compareTo(MyDate other) {
+        if (year != other.year) return Integer.compare(year, other.year);
+        if (month != other.month) return Integer.compare(month, other.month);
+        return Integer.compare(day, other.day);
+    }
+
+    @Override
+    public String toString() {
+        return day + "/" + month + "/" + year;
     }
 }
 
-class Time
-{
-    int hours, minutes;
+class MyTime implements Comparable<MyTime> {
+    private int hours, minutes;
+    private int value;//easy to use value for time
 
-    public Time(int hours, int minutes)
-    {
+    public MyTime(int hours, int minutes) {
         this.hours = hours;
         this.minutes = minutes;
+        this.value = hours * 100 + minutes;
     }
 
-    public int getHours() {return hours;}
-    public void setHours(int hours) {this.hours = hours;}
 
-    public int getMinutes() {return minutes;}
-    public void setMinutes(int minutes) {this.minutes = minutes;}
+
+    public int get_hours() {return hours;}
+    public int get_minutes() {return minutes;}
+
+    //Both setters update the corresponding parameter and the value
+    public void set_hours(int hours) {this.hours = hours;    this.value = hours * 100 + minutes;}
+    public void set_minutes(int minutes) {this.minutes = minutes;    this.value = hours * 100 + minutes;}
+
+
 
     @Override
-    public String toString()
-    {
-        return hours + ":" + minutes;
+    public int compareTo(MyTime other) {
+        return Integer.compare(this.value, other.value);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d"/*this for formating the text such that both hours and minutes have two slots*/, hours, minutes);
+    }
+}
+
+class DateTime implements Comparable<DateTime> {//to make comparing start and end more convenient
+    private MyDate date;
+    private MyTime time;
+
+    public DateTime(MyDate date, MyTime time) {
+        this.date = date;
+        this.time = time;
+    }
+
+
+    public MyDate getDate() {return date;}
+    public MyTime getTime() {return time;}
+
+    public void setDate(MyDate date) {this.date = date;}
+    public void setTime(MyTime time) {this.time = time;}
+
+
+    @Override
+    public int compareTo(DateTime other) {
+        int dateComparison = this.date.compareTo(other.date);
+
+        if (dateComparison != 0) {
+            return dateComparison;
+        }
+
+        return this.time.compareTo(other.time);
+    }
+
+    @Override
+    public String toString() {
+        return date + " " + time;
+    }
+}
+
+class Department {//Just in case so we don't lose points
+    private String name;
+    private String responsiblePerson;
+
+    public Department(String name, String responsiblePerson) {
+        this.name = name;
+        this.responsiblePerson = responsiblePerson;
+    }
+
+
+
+    public String get_name() {return name;}
+    public String get_responsible_person() {return responsiblePerson;}
+
+    public void set_name(String name) {this.name = name;}
+    public void set_responsible_person(String responsiblePerson) {this.responsiblePerson = responsiblePerson;}
+
+
+
+    @Override
+    public String toString() {
+        return name + " - Responsible: " + responsiblePerson;
+    }
+}
+
+class Venue {
+    private String name;
+    private String type;
+    private int maxCapacity;
+
+    public Venue(String name, int typeIndex, int maxCapacity) {
+        this.name = name;
+        this.type = DataStructures.VenueTypes[typeIndex];
+        this.maxCapacity = maxCapacity;
+    }
+
+
+    public String get_name() {
+        return name;
+    }
+    public String get_type() {
+        return type;
+    }
+    public int get_max_capacity() {
+        return maxCapacity;
+    }
+
+    public void set_name(String name) {
+        this.name = name;
+    }
+    public void set_type(int typeIndex) {
+        this.type = DataStructures.VenueTypes[typeIndex];
+    }
+    public void set_max_capacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+
+    @Override
+    public String toString() {
+        return name + " (" + type + "), Capacity: " + maxCapacity;
     }
 }
---
+++ ICS108Project/src/MenuManager.java
@@ -0,0 +1,451 @@
+import java.util.Scanner;
+
+public class MenuManager
+{
+    private Scanner reader;
+    private OperationsManagement operations;
+
+    public MenuManager(OperationsManagement operations) {
+        this.operations = operations;
+        this.reader = new Scanner(System.in);
+    }
+
+    public void start() {
+        MainMenu mainMenu = new MainMenu(operations, reader);
+        mainMenu.start();
+    }
+}
+
+interface Task {
+    String get_name();
+    void execute();
+}
+
+abstract class ConfirmableTask implements Task {
+    protected OperationsManagement operations;
+    protected Scanner reader;
+
+    public ConfirmableTask(OperationsManagement operations, Scanner reader) {
+        this.operations = operations;
+        this.reader = reader;
+    }
+
+    @Override
+    public final void execute() {
+        boolean successful = run_task();
+
+        if (successful) {
+            show_summary();
+
+            if (confirm()) {
+                on_confirmed();
+            } else {
+                on_cancelled();
+            }
+        }
+    }
+
+    protected abstract boolean run_task();
+
+    protected abstract void on_confirmed();
+
+    protected void show_summary() {
+        // Optional: override in child classes
+    }
+
+    protected void on_cancelled() {
+        System.out.println("Task cancelled.");
+    }
+
+    private boolean confirm() {
+        System.out.print("\nConfirm this task? (Y/n): ");
+        String choice = reader.nextLine();
+
+        return !choice.equalsIgnoreCase("n");
+    }
+}
+
+abstract class Menu {
+    protected String menuName;
+    protected Task[] tasks;
+    protected Scanner reader;
+
+    public Menu(String menuName, Scanner reader) {
+        this.menuName = menuName;
+        this.reader = reader;
+    }
+
+    public void start() {
+        boolean running = true;
+
+        while (running) {
+            view();
+
+            int choice = get_choice();
+
+            if (choice == tasks.length) {
+                running = false;
+            } else {
+                tasks[choice].execute();
+            }
+        }
+    }
+
+    protected void view() {
+        System.out.println("\n==== " + menuName + " ====");
+
+        for (int i = 0; i < tasks.length; i++) {
+            System.out.println(i + " : " + tasks[i].get_name());
+        }
+
+        System.out.println(tasks.length + " : Back/Exit");
+        System.out.print("Choose: ");
+    }
+
+    protected int get_choice() {
+        while (true) {
+            try {
+                int choice = Integer.parseInt(reader.nextLine());
+
+                if (choice >= 0 && choice <= tasks.length) {
+                    return choice;
+                }
+
+                System.out.print("Invalid choice. Try again:P : ");
+            } catch (Exception e) {
+                System.out.print("Invalid input. Try again:P : ");
+            }
+        }
+    }
+}
+
+class MainMenu extends Menu {
+    public MainMenu(OperationsManagement operations, Scanner reader) {
+        super("Main Menu", reader);
+
+        tasks = new Task[] {
+                new AddEventTask(operations, reader),
+                new ViewEventsTask(operations),
+                new AddVenueTask(operations, reader),
+                new ViewVenuesTask(operations)
+        };
+    }
+}
+
+abstract class InputBox<T> {
+    protected Scanner reader;
+    protected String label;
+
+    public InputBox(Scanner reader, String label) {
+        this.reader = reader;
+        this.label = label;
+    }
+
+    public T get_input() {
+        while (true) {
+            try {
+                T value = collect_input();
+
+                if (validate(value)) {
+                    return value;
+                }
+
+                System.out.println(get_error_message());
+            } catch (Exception e) {
+                System.out.println(get_error_message());
+            }
+        }
+    }
+
+    protected abstract T collect_input();
+
+    protected abstract boolean validate(T value);
+
+    protected abstract String get_error_message();
+}
+
+class NameInputBox extends InputBox<String> {
+    public NameInputBox(Scanner reader, String label) {
+        super(reader, label);
+    }
+
+    @Override
+    protected String collect_input() {
+        System.out.print(label + ": ");
+        return reader.nextLine();
+    }
+
+    @Override
+    protected boolean validate(String value) {
+        return value != null && !value.trim().isEmpty();
+    }
+
+    @Override
+    protected String get_error_message() {
+        return "Invalid input. " + label + " cannot be empty :P";
+    }
+}
+
+class DateInputBox extends InputBox<MyDate> {
+    public DateInputBox(Scanner reader, String label) {
+        super(reader, label);
+    }
+
+    @Override
+    protected MyDate collect_input() {
+        System.out.println("\n" + label);
+
+        System.out.print("Year: ");
+        int year = Integer.parseInt(reader.nextLine());
+
+        System.out.print("Month: ");
+        int month = Integer.parseInt(reader.nextLine());
+
+        System.out.print("Day: ");
+        int day = Integer.parseInt(reader.nextLine());
+
+        return new MyDate(day, month, year);
+    }
+
+    @Override
+    protected boolean validate(MyDate date) {
+        if (date.get_year() < 0) return false;
+
+        if (date.get_month() < 1 || date.get_month() > 12) return false;
+
+        return date.get_day() >= 1 &&
+                date.get_day() <= MyDate.max_days(date.get_year(), date.get_month());
+    }
+
+    @Override
+    protected String get_error_message() {
+        return "Invalid date. Please enter a valid date.";
+    }
+}
+
+class TimeInputBox extends InputBox<MyTime> {
+    public TimeInputBox(Scanner reader, String label) {
+        super(reader, label);
+    }
+
+    @Override
+    protected MyTime collect_input() {
+        System.out.println("\n" + label);
+
+        System.out.print("Hours: ");
+        int hours = Integer.parseInt(reader.nextLine());
+
+        System.out.print("Minutes: ");
+        int minutes = Integer.parseInt(reader.nextLine());
+
+        return new MyTime(hours, minutes);
+    }
+
+    @Override
+    protected boolean validate(MyTime time) {
+        return time.get_hours() >= 0 &&
+                time.get_hours() <= 23 &&
+                time.get_minutes() >= 0 &&
+                time.get_minutes() <= 59;
+    }
+
+    @Override
+    protected String get_error_message() {
+        return "Invalid time. Hours must be 0–23 and minutes must be 0–59 !!!";
+    }
+}
+
+class ChoiceInputBox extends InputBox<Integer> {
+    private String[] choices;
+
+    public ChoiceInputBox(Scanner reader, String label, String[] choices) {
+        super(reader, label);
+        this.choices = choices;
+    }
+
+    @Override
+    protected Integer collect_input() {
+        System.out.println("\n" + label);
+
+        for (int i = 0; i < choices.length; i++) {
+            System.out.println(i + " : " + choices[i]);
+        }
+
+        System.out.print("Choose: ");
+        return Integer.parseInt(reader.nextLine());
+    }
+
+    @Override
+    protected boolean validate(Integer value) {
+        return value >= 0 && value < choices.length;
+    }
+
+    @Override
+    protected String get_error_message() {
+        return "Invalid choice. Please select a valid option.";
+    }
+}
+
+class CapacityInputBox extends InputBox<Integer> {
+    public CapacityInputBox(Scanner reader, String label) {
+        super(reader, label);
+    }
+
+    @Override
+    protected Integer collect_input() {
+        System.out.print(label + ": ");
+        return Integer.parseInt(reader.nextLine());
+    }
+
+    @Override
+    protected boolean validate(Integer value) {
+        return value > 0;
+    }
+
+    @Override
+    protected String get_error_message() {
+        return "Invalid capacity. It must be greater than 0.";
+    }
+}
+
+class AddEventTask extends ConfirmableTask {
+    private String name;
+    private MyDate startDate;
+    private MyTime startTime;
+    private MyDate endDate;
+    private MyTime endTime;
+    private int eventTypeIndex;
+    private Venue venue;
+    private String departmentName;
+    private String responsiblePerson;
+
+    public AddEventTask(OperationsManagement operations, Scanner reader) {
+        super(operations, reader);
+    }
+
+    @Override
+    public String get_name() {
+        return "Add Event";
+    }
+
+    @Override
+    protected boolean run_task() {
+        System.out.println("\n--- Add Event ---");
+
+        name = new NameInputBox(reader, "Event Name").get_input();
+
+        startDate = new DateInputBox(reader, "Start Date").get_input();
+        startTime = new TimeInputBox(reader, "Start Time").get_input();
+
+        endDate = new DateInputBox(reader, "End Date").get_input();
+        endTime = new TimeInputBox(reader, "End Time").get_input();
+
+        eventTypeIndex = new ChoiceInputBox(reader, "Event Type", DataStructures.EventTypes).get_input();
+        venue = operations.select_venue(reader);
+
+        departmentName = new NameInputBox(reader, "Department Name").get_input();
+        responsiblePerson = new NameInputBox(reader, "Responsible Person").get_input();
+
+        return true;
+    }
+
+    @Override
+    protected void show_summary() {
+        System.out.println("\n--- Event Summary ---");
+        System.out.println("Name: " + name);
+        System.out.println("Start: " + startDate + " " + startTime);
+        System.out.println("End: " + endDate + " " + endTime);
+        System.out.println("Type: " + DataStructures.EventTypes[eventTypeIndex]);
+        System.out.println("Venue: " + venue);
+        System.out.println("Department: " + departmentName);
+        System.out.println("Responsible Person: " + responsiblePerson);
+    }
+
+    @Override
+    protected void on_confirmed() {
+        operations.add_event(
+                name,
+                startDate,
+                startTime,
+                endDate,
+                endTime,
+                eventTypeIndex,
+                venue,
+                departmentName,
+                responsiblePerson
+        );
+    }
+}
+
+class AddVenueTask extends ConfirmableTask {
+    private String name;
+    private int venueTypeIndex;
+    private int maxCapacity;
+
+    public AddVenueTask(OperationsManagement operations, Scanner reader) {
+        super(operations, reader);
+    }
+
+    @Override
+    public String get_name() {
+        return "Add Venue";
+    }
+
+    @Override
+    protected boolean run_task() {
+        System.out.println("\n--- Add Venue ---");
+        name = new NameInputBox(reader, "Venue Name").get_input();
+        venueTypeIndex = new ChoiceInputBox(reader, "Venue Type", DataStructures.VenueTypes).get_input();
+        maxCapacity = new CapacityInputBox(reader, "Maximum Capacity").get_input();
+        return true;
+    }
+
+    @Override
+    protected void show_summary() {
+        System.out.println("\n--- Venue Summary ---");
+        System.out.println("Name: " + name);
+        System.out.println("Type: " + DataStructures.VenueTypes[venueTypeIndex]);
+        System.out.println("Maximum Capacity: " + maxCapacity);
+    }
+
+    @Override
+    protected void on_confirmed() {
+        operations.add_venue(name, venueTypeIndex, maxCapacity);
+    }
+}
+
+class ViewEventsTask implements Task {
+    private OperationsManagement operations;
+
+    public ViewEventsTask(OperationsManagement operations) {
+        this.operations = operations;
+    }
+
+    @Override
+    public String get_name() {
+        return "View Events";
+    }
+
+    @Override
+    public void execute() {
+        operations.view_events();
+    }
+}
+
+class ViewVenuesTask implements Task {
+    private OperationsManagement operations;
+
+    public ViewVenuesTask(OperationsManagement operations) {
+        this.operations = operations;
+    }
+
+    @Override
+    public String get_name() {
+        return "View Venues";
+    }
+
+    @Override
+    public void execute() {
+        operations.view_venues();
+    }
+}
